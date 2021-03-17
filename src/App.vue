14febe124433c3d6f8596c9d34b117d06108e277
liuzhaozhi <1765805596@qq.com>
2021-03-17 15:17:52 +0800

    首页附近店铺样式布局

--- src/App.vue
+++ src/App.vue
@@ -99,6 +99,99 @@
       </div>
     </div>
     <div class="gap"></div>
+    <div class="nearby">
+      <h3 class="nearby__title">附近店铺</h3>
+      <div class="nearby__item">
+        <img
+          class="nearby__item__img"
+          alt="nearby__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/near.png"
+        />
+        <div class="nearby__content">
+          <div class="nearby__content__title">沃尔玛</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">月售1万+</span>
+          </div>
+          <p class="nearby__content__highlight">
+            VIP尊享满89元减4元运费券（每月3张）
+          </p>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          class="nearby__item__img"
+          alt="nearby__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/near.png"
+        />
+        <div class="nearby__content">
+          <div class="nearby__content__title">沃尔玛</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">月售1万+</span>
+          </div>
+          <p class="nearby__content__highlight">
+            VIP尊享满89元减4元运费券（每月3张）
+          </p>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          class="nearby__item__img"
+          alt="nearby__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/near.png"
+        />
+        <div class="nearby__content">
+          <div class="nearby__content__title">沃尔玛</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">月售1万+</span>
+          </div>
+          <p class="nearby__content__highlight">
+            VIP尊享满89元减4元运费券（每月3张）
+          </p>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          class="nearby__item__img"
+          alt="nearby__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/near.png"
+        />
+        <div class="nearby__content">
+          <div class="nearby__content__title">沃尔玛</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">月售1万+</span>
+          </div>
+          <p class="nearby__content__highlight">
+            VIP尊享满89元减4元运费券（每月3张）
+          </p>
+        </div>
+      </div>
+      <div class="nearby__item">
+        <img
+          class="nearby__item__img"
+          alt="nearby__item__img"
+          src="http://www.dell-lee.com/imgs/vue3/near.png"
+        />
+        <div class="nearby__content">
+          <div class="nearby__content__title">沃尔玛</div>
+          <div class="nearby__content__tags">
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">月售1万+</span>
+            <span class="nearby__content__tag">月售1万+</span>
+          </div>
+          <p class="nearby__content__highlight">
+            VIP尊享满89元减4元运费券（每月3张）
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
   <div class="docker">
     <div class="docker__item docker__item--active">
@@ -124,12 +217,13 @@
 @import "style/viriables"
 @import "style/mixins"
 .wrapper
+  overflow-y: auto
   position: absolute
   top: 0
   right: 0
   bottom: .5rem
   left: 0
-  padding: 0 .18rem
+  padding: 0 .18rem .1rem .18rem
 
   .position
     position: relative
@@ -199,6 +293,49 @@
     margin: 0 -.18rem
     height: .1rem
     background: $content-bgColor
+
+  .nearby
+    &__title
+      margin: .16rem 0 .02rem 0
+      font-size: .18rem
+      font-weight: normal
+      color: $content-fontColor
+
+    &__item
+      display: flex
+      padding-top: .12rem
+
+      &__img
+        margin-right: .16rem
+        width: .56rem
+        height: .56rem
+
+    &__content
+      flex: 1
+      padding-bottom: .12rem
+      border-bottom: 1px solid $content-bgColor
+
+
+      &__title
+        line-height: .22rem
+        font-size: .16rem
+        color: $content-fontColor
+
+      &__tags
+        margin-top: .08rem
+        line-height: .18rem
+        font-size: .13rem
+        color: $content-fontColor
+
+      &__tag
+        margin-right: .16rem
+
+      &__highlight
+        margin: .08rem 0 0 0
+        line-height: .18rem
+        font-size: .13rem
+        color: #E93B3B
+
 
 .docker
   display: flex
