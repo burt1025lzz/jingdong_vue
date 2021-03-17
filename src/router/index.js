bfa8198c0b9aadbe2e5c056f25890cff853c96dd
liuzhaozhi <1765805596@qq.com>
2021-03-17 18:03:14 +0800

    路由守卫实现基础登陆校验功能

--- src/router/index.js
+++ src/router/index.js
@@ -11,22 +11,22 @@ const routes = [
   {
     path: "/login",
     name: "Login",
-    component: Login
+    component: Login,
+    beforeEnter(to, from, next) {
+      const { isLogin } = localStorage;
+      isLogin ? next({ name: "Home" }) : next();
+    }
   }
-  // {
-  //   path: "/about",
-  //   name: "About",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
-  // }
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  const { isLogin } = localStorage;
+  isLogin || to.name === "Login" ? next() : next({ name: "Login" });
 });
 
 export default router;
