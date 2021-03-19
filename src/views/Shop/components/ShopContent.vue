01c61ff373469d0df34e3bff76de67ca7f10a60b
liuzhaozhi <1765805596@qq.com>
2021-03-19 17:05:30 +0800

    使用watchEffect抽离代码

--- src/views/Shop/components/ShopContent.vue
+++ src/views/Shop/components/ShopContent.vue
@@ -1,26 +1,31 @@
 <template>
   <div class="content">
     <div class="category">
-      <div class="category__item category__item--active">全部商品</div>
-      <div class="category__item">秒杀</div>
-      <div class="category__item">新鲜水果</div>
-      <div class="category__item">休闲食品</div>
-      <div class="category__item">时令蔬菜</div>
-      <div class="category__item">肉蛋家禽</div>
+      <div
+        :class="{
+          category__item: true,
+          'category__item--active': currentTab === item.tab
+        }"
+        v-for="item in categories"
+        :key="item.tab"
+        @click="() => handleTabClick(item.tab)"
+      >
+        {{ item.name }}
+      </div>
     </div>
     <div class="product">
-      <div class="product__item">
+      <div class="product__item" v-for="item in list" :key="item._id">
         <img
           src="http://www.dell-lee.com/imgs/vue3/near.png"
           alt="product__item__img"
           class="product__item__img"
         />
         <div class="product__item__detail">
-          <h4 class="product__item__title">番茄250g/份</h4>
-          <p class="product__item__sales">月售10件</p>
+          <h4 class="product__item__title">{{ item.name }}</h4>
+          <p class="product__item__sales">月售{{ item.sales }}件</p>
           <p class="product__item__price">
-            <span class="product__item__yen">&yen;</span>33.6
-            <span class="product__item__origin">&yen;66.6</span>
+            <span class="product__item__yen">&yen;</span>{{ item.price }}
+            <span class="product__item__origin">&yen;{{ item.oldPrice }}</span>
           </p>
         </div>
         <div class="product__number">
@@ -34,8 +39,51 @@
 </template>
 
 <script>
+import { reactive, ref, toRefs, watchEffect } from "vue";
+import { get } from "@/utils/request";
+import { useRoute } from "vue-router";
+
+const categories = [
+  { name: "全部商品", tab: "all" },
+  { name: "秒杀", tab: "seckill" },
+  { name: "新鲜水果", tab: "fruit" }
+];
+
+const useTabEffect = () => {
+  const currentTab = ref(categories[0].tab);
+  const handleTabClick = tab => {
+    currentTab.value = tab;
+  };
+  return { currentTab, handleTabClick };
+};
+
+const useCurrentListEffect = currentTab => {
+  const route = useRoute();
+  const routeId = route.params.id;
+  const content = reactive({ list: [] });
+  const getContentData = () => {
+    get(`/api/shop/${routeId}/products`, {
+      tab: currentTab.value
+    }).then(resp => {
+      if (resp?.errno === 0 && resp?.data.length) {
+        content.list = resp.data;
+      }
+    });
+  };
+  watchEffect(() => {
+    getContentData();
+  });
+  const { list } = toRefs(content);
+  return { list };
+};
 export default {
-  name: "ShopContent"
+  name: "ShopContent",
+  setup() {
+    const { currentTab, handleTabClick } = useTabEffect();
+    const { list } = useCurrentListEffect(currentTab);
+
+    return { list, categories, currentTab, handleTabClick };
+  }
 };
 </script>
 
